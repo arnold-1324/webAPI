74e8980aece8425d5bba8f2f5e4ebc774580db57
Arnold Gnanaselvam <80133@SHSSDOM.COM>
2024-10-14 15:38:09 +0530

    models

---
+++ Models/User.cs
@@ -0,0 +1,51 @@
+using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+namespace twitterclone.Models;
+
+public class User
+{
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        
+        public  required string Id { get; set; }
+
+        [BsonRequired]
+        public required string Username { get; set; }
+
+        [BsonRequired]
+        public required string FullName { get; set; }
+
+        [BsonRequired]
+         [MinLength(6, ErrorMessage = "Password must be at least 6 characters long.")]
+        public required string Password { get; set; }
+
+        public DateTime LastLogin { get; set; } = DateTime.Now;
+
+        public bool IsVerified { get; set; } = false;
+
+        public required string ResetPasswordToken { get; set; }
+        public DateTime? ResetPasswordExpires { get; set; }
+
+        public required string VerificationToken { get; set; }
+        public DateTime? VerificationTokenExpiresAt { get; set; }
+
+        [BsonRequired]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")] 
+        //[UniqueEmail]
+        public required string Email { get; set; }
+
+        public List<string> Followers { get; set; } = new List<string>();
+        public List<string> Following { get; set; } = new List<string>();
+        public bool IsFrozen { get; set; } = false;
+
+        public string ProfileImg { get; set; } = "";
+        public string Bio { get; set; } = "";
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,61 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddEnvironmentVariables();
+
+
+builder.Services.AddSingleton<IMongoClient>(new MongoClient(builder.Configuration["MongoDBSettings:ConnectionString"]));
+
+
+builder.Services.AddControllers();
+
+
+builder.Services.AddHttpContextAccessor();
+
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+        };
+    });
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(); 
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers(); 
+
+
+
+
+app.MapGet("/", () => "Welcome to the API!");
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+     
+    app.UseSwaggerUI();
+}
+
 
 app.Run();
